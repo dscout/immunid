b642731d34c893e29662c5a15cc0c10dcf5a46ba
Parker Selbert <parker@sorentwo.com>
2015-01-21 11:31:08 -0600

    Expose Relation at the top level

--- immunid.js
+++ immunid.js
@@ -1,3 +1,4 @@
-exports.Adapter = require('./lib/Adapter');
-exports.Model   = require('./lib/Model');
-exports.Store   = require('./lib/Store');
+exports.Adapter  = require('./lib/Adapter');
+exports.Model    = require('./lib/Model');
+exports.Relation = require('./lib/Relation');
+exports.Store    = require('./lib/Store');
