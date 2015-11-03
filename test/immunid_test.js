0d7634ea0cfe3f952daf90965f52cc5678386b17
Taylor Briggs <taylor.briggs@gmail.com>
2015-11-03 16:27:18 -0600

    Export the events module

--- test/immunid_test.js
+++ test/immunid_test.js
@@ -3,6 +3,10 @@ var Immunid = require('../immunid');
 
 describe('Immunid', function() {
   it('defines all sub-modules on the required object', function() {
-    expect(Immunid.Model).not.to.be.undefined;
+    expect(Immunid.Model).to.exist;
+    expect(Immunid.Store).to.exist;
+    expect(Immunid.Adapter).to.exist;
+    expect(Immunid.Relation).to.exist;
+    expect(Immunid.Events).to.exist;
   });
 });
